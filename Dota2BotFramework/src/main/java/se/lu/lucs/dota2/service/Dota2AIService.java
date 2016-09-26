fec3ac829fa0953180b68ca482aa9787181a60b5
Tobias Mahlmann <t.mahlmann@gmail.com>
2016-09-26 14:15:51 +0300

    Replaced the dependency on Apache Tomcat by a much simpler integrated solution based on NanoHTTP
    Raised webservice version to 0.0.2-SNAPSHOT

--- Dota2BotFramework/src/main/java/se/lu/lucs/dota2/service/Dota2AIService.java
+++ Dota2BotFramework/src/main/java/se/lu/lucs/dota2/service/Dota2AIService.java
@@ -1,17 +1,15 @@
 package se.lu.lucs.dota2.service;
 
-import java.util.ServiceLoader;
+import java.io.IOException;
+import java.util.Map;
 import java.util.logging.Logger;
 
-import javax.inject.Singleton;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import fi.iki.elonen.NanoHTTPD;
 import se.lu.lucs.dota2.framework.bot.Bot;
 import se.lu.lucs.dota2.framework.bot.Bot.Command;
 import se.lu.lucs.dota2.framework.bot.BotCommands.LevelUp;
@@ -19,75 +17,176 @@ import se.lu.lucs.dota2.framework.bot.BotCommands.Select;
 import se.lu.lucs.dota2.framework.game.ChatEvent;
 import se.lu.lucs.dota2.framework.game.World;
 
-@Path( "/service" )
-@Singleton
-public class Dota2AIService {
-    private static final Logger LOGGER = Logger.getLogger( Dota2AIService.class.getName() );
-    private final Bot bot;
+/**
+ * This class uses NanoHTTD to provide a very simple webservice The webservice
+ * binds to port 8080 and loads an instance of the Bot class supplied as the
+ * first argument. It does not bind to specific URL yet, but only switches based
+ * on the last part of the requested URL. Hence running multiple bots requires
+ * multiple instances of this service yet.
+ *
+ * @author Tobias Mahlmann
+ *
+ */
+public class Dota2AIService extends NanoHTTPD {
 
-    public Dota2AIService() {
-        final ServiceLoader<Bot> botLoader = ServiceLoader.load( Bot.class );
-        bot = botLoader.iterator().next();
-        LOGGER.fine( "Dota2AIService created" );
-    }
+	private final static ObjectMapper MAPPER = new ObjectMapper();
 
-    @POST
-    @Path( "/chat" )
-    @Consumes( MediaType.APPLICATION_JSON )
-    public Response chat( ChatEvent e ) {
-        bot.onChat( e );
-        return Response.status( Status.OK ).build();
-    }
+	private static final Logger LOGGER = Logger.getLogger(Dota2AIService.class.getName());
 
-    @POST
-    @Path( "/levelup" )
-    @Produces( MediaType.APPLICATION_JSON )
-    public Response levelUp() {
-        final LevelUp up = bot.levelUp();
-        return Response.status( Status.OK ).entity( up ).build();
-    }
+	private final static String ACCEPT_FIELD = "accept";
 
-    @POST
-    @Path( "/reset" )
-    @Consumes( MediaType.APPLICATION_JSON )
-    public Response reset( String input ) {
-        bot.reset();
-        return Response.status( Status.OK ).build();
-    }
+	private final static String APPLICATION_JSON = "application/json";
 
-    @POST
-    @Path( "/select" )
-    @Produces( MediaType.APPLICATION_JSON )
-    public Response select( Select e ) {
-        final Select s = bot.select();
-        LOGGER.info( "Select was called. We returned " + s.getHero() );
-        return Response.status( Status.OK ).entity( s ).build();
-    }
+	private final static String CONTENT_TYPE = "content/type";
 
-    @POST
-    @Path( "/test" )
-    @Consumes( MediaType.APPLICATION_JSON )
-    public Response test( String input ) {
-        System.out.println( input );
-        return Response.status( Status.OK ).build();
-    }
+	public static void main(String[] args)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+		if (args.length < 1) {
+			System.err.println("First argument must be FQN of your bot class");
+			return;
+		}
 
-    @POST
-    @Path( "/update" )
-    @Consumes( MediaType.APPLICATION_JSON )
-    @Produces( MediaType.APPLICATION_JSON )
-    public Response update( World world ) {
-        try {
-            final Command command = bot.update( world );
+		final Class<Bot> botClass = (Class<Bot>) Class.forName(args[0]);
+		new Dota2AIService(botClass.newInstance());
 
-            // return HTTP response 200 in case of success
-            return Response.status( Status.OK ).entity( command ).build();
-        }
-        catch (final Exception e) {
-            System.out.println( world.toString() );
+	}
 
-            throw e;
-        }
-    }
+	private final Bot bot;
 
+	public Dota2AIService(Bot bot) throws IOException {
+		super(8080);
+		this.bot = bot;
+		start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+		LOGGER.fine("Dota2AIService created");
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see fi.iki.elonen.NanoHTTPD#serve(fi.iki.elonen.NanoHTTPD.IHTTPSession)
+	 */
+	@Override
+	public Response serve(IHTTPSession session) {
+		// This method does a few sanity checks and then calls the respective
+		// method
+		// based on the requested URL. These methods then build the response
+		if (session.getMethod() != Method.POST) {
+			return newFixedLengthResponse(Response.Status.METHOD_NOT_ALLOWED, "text/plain", "Only POST allowed");
+		}
+
+		final String method = session.getUri().substring(session.getUri().lastIndexOf('/') + 1);
+		Response res;
+
+		try {
+			switch (method) {
+			case "chat":
+				res = requireJSON(session);
+				if (res != null) {
+					break;
+				}
+				chat(session);
+				res = newFixedLengthResponse("");
+				break;
+			case "reset":
+				reset(session);
+				res = newFixedLengthResponse("");
+				break;
+			case "levelup":
+				res = levelup(session);
+				break;
+			case "select":
+				res = select(session);
+				break;
+			case "update":
+				res = requireJSON(session);
+				if (res != null) {
+					break;
+				}
+				res = update(session);
+				break;
+			default:
+				res = newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "method not found");
+				break;
+
+			}
+		} catch (final Exception e) {
+			res = newFixedLengthResponse(Response.Status.INTERNAL_ERROR, MIME_PLAINTEXT, e.getMessage());
+		}
+		return res;
+	}
+
+	/**
+	 * Ensures that the "accept" header is either set to JSON or empty
+	 * 
+	 * @param session
+	 * @return
+	 */
+	private Response assureAcceptJSON(IHTTPSession session) {
+		final Map<String, String> headers = session.getHeaders();
+		if (!APPLICATION_JSON.equals(headers.get(ACCEPT_FIELD))) {
+			return newFixedLengthResponse(Response.Status.NOT_ACCEPTABLE, MIME_PLAINTEXT,
+					"set accept to application/json or remove it");
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Helper method to serialize a POJO into JSON
+	 * 
+	 * @param o
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	private Response buildJSONResponse(Object o) throws JsonProcessingException {
+		return newFixedLengthResponse(MAPPER.writeValueAsString(o));
+
+	}
+
+	private void chat(IHTTPSession session) throws JsonParseException, JsonMappingException, IOException {
+		final ChatEvent e = MAPPER.readValue(session.getInputStream(), ChatEvent.class);
+		bot.onChat(e);
+	}
+
+	private Response levelup(IHTTPSession session) throws JsonProcessingException {
+		final Response res = assureAcceptJSON(session);
+		if (res != null) {
+			return res;
+		}
+		final LevelUp l = bot.levelUp();
+		return buildJSONResponse(l);
+	}
+
+	/**
+	 * Ensures that the supplied data has the "content-type" set to JSON
+	 * 
+	 * @param session
+	 * @return
+	 */
+	private Response requireJSON(IHTTPSession session) {
+		final Map<String, String> headers = session.getHeaders();
+		if (!APPLICATION_JSON.equals(headers.get(CONTENT_TYPE))) {
+			return newFixedLengthResponse(Response.Status.NOT_ACCEPTABLE, MIME_PLAINTEXT,
+					"Set content-type to application/json");
+		} else {
+			return null;
+		}
+	}
+
+	private void reset(IHTTPSession session) {
+		bot.reset();
+	}
+
+	private Response select(IHTTPSession session) throws JsonProcessingException {
+		final Select s = bot.select();
+		LOGGER.info("Select was called. We returned " + s.getHero());
+		return buildJSONResponse(s);
+	}
+
+	private Response update(IHTTPSession session) throws IOException {
+		final World world = MAPPER.readValue(session.getInputStream(), World.class);
+		final Command c = bot.update(world);
+		return buildJSONResponse(c);
+	}
 }
